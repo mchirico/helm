51089352e8e7279cdb3f1db45e4c565e6436e247
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-11-14 12:04:33 -0500

    fix

--- examples/pulsar/google-kubernetes-engine/name.sh
+++ examples/pulsar/google-kubernetes-engine/name.sh
@@ -7,7 +7,7 @@ bin/pulsar initialize-cluster-metadata \
   --web-service-url http://broker.default.svc.cluster.local:8080/ \
   --broker-service-url pulsar://broker.default.svc.cluster.local:6650/
 
-
+alias pulsar-admin='kubectl exec pulsar-admin -it -- bin/pulsar-admin'
 pulsar-admin tenants create ten --admin-roles admin --allowed-clusters pulsar-gke
 pulsar-admin namespaces create ten/ns
 
@@ -15,4 +15,8 @@ pulsar-admin namespaces delete tt/ns
 pulsar-admin tenants delete tt
 
 
+
+alias pulsar-perf='kubectl exec pulsar-admin -it -- bin/pulsar-perf'
+pulsar-perf produce persistent://ten/us-central/ns/my-topic \
+  --rate 10000
 
